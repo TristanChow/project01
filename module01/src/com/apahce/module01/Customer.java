b1bea3c9be677fac90f969e37016ee1a84b65547
TristanChow <869007664@qq.com>
2019-02-25 00:03:42 +0800

    Initial commit

---
+++ module01/src/com/apahce/module01/Customer.java
@@ -0,0 +1,8 @@
+package com.apahce.module01;
+
+public class Customer {
+
+    public static void main(String[] args){
+        System.out.println(123);
+    }
+}
---
+++ src/com/apache/java/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.apache.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("XXXssssX");
+        int i = 0;
+        ArrayList list = new ArrayList();
+        
+        Date date = new Date();
+
+
+ }
+}
+
+
+
+
